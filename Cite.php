4c4e35e88127f3ea6c771c135ee69a6cf4f7629a
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-12-23 02:34:39 +0000

    * $wf => $wg * give <ref> and <references> a css class, reference and references, respectively * Code cleanup * Lots of documentation, every function fully documented + code comments * Split link() into linkRef() and linkReferences() * Split key() into refKey() and referencesKey()

--- Cite.php
+++ Cite.php
@@ -21,7 +21,7 @@ $wgExtensionCredits['parserhook'][] = array(
 /**
  * Error codes, first array = internal errors; second array = user errors
  */
-$wfCiteErrors = array(
+$wgCiteErrors = array(
 	'system' => array(
 		'CITE_ERROR_STR_INVALID',
 		'CITE_ERROR_KEY_INVALID_1',
@@ -38,12 +38,12 @@ $wfCiteErrors = array(
 	)
 );
 
-for ( $i = 0; $i < count( $wfCiteErrors['system'] ); ++$i )
+for ( $i = 0; $i < count( $wgCiteErrors['system'] ); ++$i )
 	// System errors are negative integers
-	define( $wfCiteErrors['system'][$i], -($i + 1) );
-for ( $i = 0; $i < count( $wfCiteErrors['user'] ); ++$i )
+	define( $wgCiteErrors['system'][$i], -($i + 1) );
+for ( $i = 0; $i < count( $wgCiteErrors['user'] ); ++$i )
 	// User errors are positive integers
-	define( $wfCiteErrors['user'][$i], $i + 1 );
+	define( $wgCiteErrors['user'][$i], $i + 1 );
 
 function wfCite() {
 	global $wgMessageCache;
@@ -74,25 +74,80 @@ function wfCite() {
 			'cite_references_link_prefix' => '_note_',
 			'cite_references_link_suffix' => '',
 
-			'cite_reference_link' => '<sup id="$1">[[#$2|<nowiki>[</nowiki>$3<nowiki>]</nowiki>]]</sup>',
+			'cite_reference_link' => '<sup id="$1" class="reference">[[#$2|<nowiki>[</nowiki>$3<nowiki>]</nowiki>]]</sup>',
 			'cite_references_link_one' => '<li><cite id="$1">[[#$2|^]] $3</cite></li>',
 			'cite_references_link_many' => '<li>^ <cite id="$1">$2 $3</cite></li>',
 			'cite_references_link_many_format' => '[[#$1|<sup>$2</sup>]]',
 			'cite_references_link_many_sep' => "\xc2\xa0", // &nbsp;
 			'cite_references_link_many_and' => "\xc2\xa0", // &nbps;
 
-			'cite_references_prefix' => '<ol>',
+			// Although I could just use # instead of <li> above and nothing here that
+			// will break on input that contains linebreaks
+			'cite_references_prefix' => '<ol class="references">',
 			'cite_references_suffix' => '</ol>',
 		)
 	);
 	
 	class Cite {
-		/**#@+ @access private */
+		/**#@+
+		 * @access private
+		 */
+		
+		/**
+		 * Datastructure representing <ref> input, in the format of:
+		 * <code>
+		 * array(
+		 * 	'user supplied' => array(
+		 *		'text' => 'user supplied reference & key',
+		 *		'count' => 1, // occurs twice
+		 *	),
+		 *	0 => 'Anonymous reference',
+		 *	1 => 'Another anonymous reference',
+		 *	'some key' => array(
+		 *		'text' => 'this one occurs once'
+		 *		'count' => 0
+		 *	),
+		 *	3 => 'more stuff'
+		 * );
+		 * </code>
+		 *
+		 * This works because:
+		 * * PHP's datastructures are guarenteed to be returned in the
+		 *   order that things are inserted into them (unless you mess
+		 *   with that)
+		 * * User supplied keys can't be integers, therefore avoiding
+		 *   conflict with anonymous keys
+		 *
+		 * @var array
+		 **/
 		var $mRefs = array();
-		var $mI = 0, $mJ = 0;
+		
+		/**
+		 * Count for user displayed output (ref[1], ref[2], ...)
+		 *
+		 * @var int
+		 */
+		var $mOutCnt = 0;
+
+		/**
+		 * Internal counter for anonymous references, seperate from
+		 * $mOutCnt because anonymous references won't increment it,
+		 * but will incremement $mOutCnt
+		 *
+		 * @var int
+		 */
+		var $mInCnt = 0;
+		
+		/**
+		 * @var object
+		 */
 		var $mParser, $mParserOptions;
+		
 		/**#@-*/
 
+		/**
+		 * Constructor
+		 */
 		function Cite() {
 			$this->setHooks();
 			$this->genParser();
@@ -100,6 +155,13 @@ function wfCite() {
 
 		/**#@+ @access private */
 
+		/**
+		 * Callback function for <ref>
+		 *
+		 * @param string $str Input
+		 * @param array $argv Arguments
+		 * @return string
+		 */
 		function ref( $str, $argv ) {
 			$key = $this->refArg( $argv );
 			
@@ -107,6 +169,9 @@ function wfCite() {
 				if ( $str === '' )
 					return $this->error( CITE_ERROR_REF_NO_INPUT );
 				if ( is_string( $key ) )
+					// I don't want keys in the form of /^[0-9]+$/ because they would
+					// conflict with the php datastructure I'm using, besides, why specify
+					// a manual key if it's just going to be any old integer?
 					if ( sprintf( '%d', $key ) === (string)$key )
 						return $this->error( CITE_ERROR_REF_NUMERIC_KEY );
 					else
@@ -137,8 +202,6 @@ function wfCite() {
 		/**
 		 * Parse the arguments to the <ref> tag
 		 *
-		 * @access private
-		 *
 		 * @param array $argv The argument vector
 		 *
 		 * @return mixed false on invalid input, a string on valid
@@ -167,7 +230,7 @@ function wfCite() {
 			if ( $key === null ) {
 				// No key
 				$this->mRefs[] = $str;
-				return $this->link( $this->mJ++, $str );
+				return $this->linkRef( $this->mInCnt++ );
 			} else if ( is_string( $key ) )
 				// Valid key
 				if ( ! @is_array( $this->mRefs[$key] ) ) {
@@ -176,15 +239,19 @@ function wfCite() {
 						'text' => $str,
 						'count' => 0
 					);
-					return $this->link( $key, $str, 0 );
+					return $this->linkRef( $key, 0 );
 				} else
 					// We've been here before
-					return $this->link( $key, $str, ++$this->mRefs[$key]['count'] );
+					return $this->linkRef( $key, ++$this->mRefs[$key]['count'] );
 			else
 				$this->croak( CITE_ERROR_STACK_INVALID_INPUT, serialize( array( $key, $str ) ) );
 		}
 		
 		/**
+		 * Callback function for <references>
+		 *
+		 * @param string $str Input
+		 * @param array $argv Arguments
 		 * @return string
 		 */
 		function references( $str, $argv ) {
@@ -196,6 +263,11 @@ function wfCite() {
 				return $this->referencesFormat();
 		}
 
+		/**
+		 * Make output to be returned from the references() function
+		 *
+		 * @return string XHTML ready for output
+		 */
 		function referencesFormat() {
 			$ent = array();
 			
@@ -209,6 +281,15 @@ function wfCite() {
 			return $this->parse( $prefix . $content . $suffix );
 		}
 
+		/**
+		 * Format a single entry for the referencesFormat() function
+		 *
+		 * @param string $key The key of the reference
+		 * @param mixed $val The value of the reference, string for anonymous
+		 *                   references, array for user-suppplied
+		 *
+		 * @return string Wikitext
+		 */
 		function referencesFormatEntry( $key, $val ) {
 			global $wgContLang;
 			
@@ -216,8 +297,8 @@ function wfCite() {
 				return
 					wfMsgForContentNoTrans(
 						'cite_references_link_one',
-						$this->key( $key, false ),
-						$this->key( $key, true ),
+						$this->referencesKey( $key ),
+						$this->refKey( $key ),
 						$val
 					);
 			else {
@@ -226,7 +307,7 @@ function wfCite() {
 				for ( $i = 0; $i <= $val['count']; ++$i ) {
 					$links[] = wfMsgForContentNoTrans(
 							'cite_references_link_many_format',
-							$this->key( $key, true, $i ),
+							$this->refKey( $key, $i ),
 							$wgContLang->formatNum( $i + 1 )
 					);
 				}
@@ -235,41 +316,69 @@ function wfCite() {
 
 				return
 					wfMsgForContentNoTrans( 'cite_references_link_many',
-						$this->key( $key, false ),
+						$this->referencesKey( $key ),
 						$list,
 						$val['text']
 					);
 			}
 		}
 
-		function key( $key, $ref, $num = null ) {
-			if ( $ref === true ) {
-				// _ref
-				$prefix = wfMsgForContent( 'cite_reference_link_prefix' );
-				$suffix = wfMsgForContent( 'cite_reference_link_suffix' );
-				if ( isset( $num ) )
-					$key = wfMsgForContentNoTrans( 'cite_reference_link_key_with_num', $key, $num );
-			} else if ( $ref === false ) {
-				// _note
-				$prefix = wfMsgForContent( 'cite_references_link_prefix' );
-				$suffix = wfMsgForContent( 'cite_references_link_suffix' );
-				if ( isset( $num ) )
-					$key = wfMsgForContentNoTrans( 'cite_reference_link_key_with_num', $key, $num );
-			}
-
+		/**
+		 * Return an id for use in wikitext output based on a key and
+		 * optionally the # of it, used in <references>, not <ref>
+		 * (since otherwise it would link to itself)
+		 *
+		 * @param string $key The key
+		 * @param int $num The number of the key
+		 * @return string A key for use in wikitext
+		 */
+		function refKey( $key, $num = null ) {
+			$prefix = wfMsgForContent( 'cite_reference_link_prefix' );
+			$suffix = wfMsgForContent( 'cite_reference_link_suffix' );
+			if ( isset( $num ) )
+				$key = wfMsgForContentNoTrans( 'cite_reference_link_key_with_num', $key, $num );
+			
 			return $prefix . $key . $suffix;
 		}
 
-		function link( $key, $val, $num = null ) {
+		/**
+		 * Return an id for use in wikitext output based on a key and
+		 * optionally the # of it, used in <references>, not <ref>
+		 * (since otherwise it would link to itself)
+		 *
+		 * @param string $key The key
+		 * @param int $num The number of the key
+		 * @return string A key for use in wikitext
+		 */
+		function referencesKey( $key, $num = null ) {
+			$prefix = wfMsgForContent( 'cite_references_link_prefix' );
+			$suffix = wfMsgForContent( 'cite_references_link_suffix' );
+			if ( isset( $num ) )
+				$key = wfMsgForContentNoTrans( 'cite_reference_link_key_with_num', $key, $num );
+			
+			return $prefix . $key . $suffix;
+		}
+
+		/**
+		 * Generate a link (<sup ...) for the <ref> element from a key
+		 * and return XHTML ready for output
+		 *
+		 * @param string $key The key for the link
+		 * @param int $num The # of the key, used for distinguishing
+		 *                 multiple occourances of the same key
+		 *
+		 * @return string
+		 */
+		function linkRef( $key, $num = null ) {
 			global $wgContLang;
 			
 			return
 				$this->parse(
 					wfMsgForContentNoTrans(
 						'cite_reference_link',
-						$this->key( $key, true, $num ),
-						$this->key( $key, false ),
-						$wgContLang->formatNum( ++$this->mI )
+						$this->refKey( $key, $num ),
+						$this->referencesKey( $key ),
+						$wgContLang->formatNum( ++$this->mOutCnt )
 					)
 				);
 		}
@@ -281,7 +390,7 @@ function wfCite() {
 		 * first seperator and not 'and' as the second, and this has to
 		 * use messages from the content language) I'm rolling my own.
 		 *
-		 * @param array $l
+		 * @param array $arr The array to format
 		 * @return string
 		 */
 		function listToText( $arr ) {
@@ -291,6 +400,7 @@ function wfCite() {
 			$and = wfMsgForContentNoTrans( 'cite_references_link_many_and' );
 
 			if ( $cnt == 1 )
+				// Enforce always returning a string
 				return (string)$arr[0];
 			else {
 				$t = array_slice( $arr, 0, $cnt - 1 );
@@ -298,6 +408,13 @@ function wfCite() {
 			}
 		}
 
+		/**
+		 * Parse a given fragment and fix up Tidy's trail of blood on
+		 * it...
+		 *
+		 * @param string $in The text to parse
+		 * @return string The parsed text
+		 */
 		function parse( $in ) {
 			global $wgTitle;
 
@@ -305,18 +422,23 @@ function wfCite() {
 			$text = $ret->getText();
 			
 			return $this->fixTidy( $text );
-			// This had trouble with including stuff
-			//return $wgOut->parse( $in, false );
 		}
 
+		/**
+		 * Tidy treats all input as a block, it will e.g. wrap most
+		 * input in <p> if it isn't already, fix that and return the fixed text
+		 *
+		 * @param string $text The text to fix
+		 * @return string The fixed text
+		 */
 		function fixTidy( $text ) {
 			global $wgUseTidy;
 
 			if ( ! $wgUseTidy )
 				return $text;
 			else {
-				$text = preg_replace( '/^<p>\s*/', '', $text );
-				$text = preg_replace( '/\s*<\/p>\s*/', '', $text );
+				$text = preg_replace( '#^<p>\s*#', '', $text );
+				$text = preg_replace( '#\s*</p>\s*#', '', $text );
 				
 				wfDebugLog( 'misc', "'''$text'''" );
 
@@ -324,6 +446,11 @@ function wfCite() {
 			}
 		}
 
+		/**
+		 * $wgOut->parse() has issues with the elements defined in
+		 * setHooks() being used inside includes templates so I'm
+		 * rolling my own parser
+		 */
 		function genParser() {
 			$this->mParser = new Parser;
 			$this->mParserOptions = new ParserOptions;
@@ -340,8 +467,10 @@ function wfCite() {
 		}
 
 		/**
+		 * Return an error message based on an error ID
+		 *
 		 * @param int $id ID for the error
-		 * @return string
+		 * @return string XHTML ready for output
 		 */
 		function error( $id ) {
 			if ( $id > 0 )

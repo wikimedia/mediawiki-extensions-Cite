7957c02207f83a6396a73941e81665093201225a
Rotem Liss <rotem@users.mediawiki.org>
2008-03-27 19:04:39 +0000

    Update for he.

--- Cite.i18n.php
+++ Cite.i18n.php
@@ -377,7 +377,7 @@ $messages['gu'] = array(
 
 /** Hebrew (עברית) */
 $messages['he'] = array(
-	'cite_desc'                                => 'מוסיף תגיות <nowiki><ref[ name=id]></nowiki> and <nowiki><references/></nowiki>, עבור הערות שוליים',
+	'cite_desc'                                => 'הוספת תגיות <nowiki><ref[ name=id]></nowiki> ו־<nowiki><references/></nowiki> עבור הערות שוליים',
 	'cite_croak'                               => 'בהערה יש שגיאה; $1: $2',
 	'cite_error_key_str_invalid'               => 'שגיאה פנימית; $str ו/או $key שגויים. זהו באג בתוכנה.',
 	'cite_error_stack_invalid_input'           => 'שגיאה פנימית; מפתח שגוי במחסנית. זהו באג בתוכנה.',

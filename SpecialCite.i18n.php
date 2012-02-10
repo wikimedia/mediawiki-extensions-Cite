ae8a0a1cb7e4b42d79139a9869eb217ba859ba7b
Raimond Spekking <raymond@users.mediawiki.org>
2012-02-10 20:57:24 +0000

    Localisation updates for core and extension messages from translatewiki.net

--- SpecialCite.i18n.php
+++ SpecialCite.i18n.php
@@ -674,11 +674,12 @@ $messages['gsw'] = array(
 );
 
 /** Gujarati (ગુજરાતી)
+ * @author Dsvyas
  * @author KartikMistry
  * @author Sushant savla
  */
 $messages['gu'] = array(
-	'cite_article_desc' => '[[Special:Cite|સંદર્ભ]] ખાસ પાનું અને ટૂલબોક્સ કડી ઉમેરે છે',
+	'cite_article_desc' => '[[Special:Cite|સંદર્ભ]] ખાસ પાનું અને સાધન પેટીની કડી ઉમેરે છે',
 	'cite_article_link' => 'આ પાનું ટાંકો',
 	'tooltip-cite-article' => 'આ પાનાંને સમર્થન કઈ રીતે આપવું તેની માહિતી',
 	'cite' => 'ટાંકો',

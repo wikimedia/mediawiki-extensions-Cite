798b4537712ea448b863eaf57800dfec685d6ff7
Alex Monk <krenair@wikimedia.org>
2014-06-23 22:42:17 +0100

    If there's no main <references /> block on the page, show one automatically below the content
    Rather than showing an error
    
    Bug: 66860
    Change-Id: I9c7c13269d5ec355ccb280402aa8c0cedd208883

--- Cite_body.php
+++ Cite_body.php
@@ -1071,11 +1071,10 @@ class Cite {
 			if ( count( $refs ) == 0 ) {
 				continue;
 			}
-			$text .= "\n<br />";
 			if ( $group == CITE_DEFAULT_GROUP ) {
-				$text .= $this->error( 'cite_error_refs_without_references' );
+				$text .= $this->referencesFormat( $group, '', '' );
 			} else {
-				$text .= $this->error( 'cite_error_group_refs_without_references', htmlspecialchars( $group ) );
+				$text .= "\n<br />" . $this->error( 'cite_error_group_refs_without_references', htmlspecialchars( $group ) );
 			}
 		}
 		return true;

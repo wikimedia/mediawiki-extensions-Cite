8b0e9c728c37e59d7e19dd5dc61ab7809268cc82
Ed Sanders <esanders@wikimedia.org>
2016-02-05 20:54:55 +0000

    VisualEditor: Bring in unit tests from VE
    Change-Id: I7da5f25f61d0c7f2331ab927c35d8f6a975041b8

--- CiteHooks.php
+++ CiteHooks.php
@@ -28,4 +28,41 @@ class CiteHooks {
 
 		return true;
 	}
+
+	/**
+	 * Conditionally register the unit testing module for the ext.cite.visualEditor module
+	 * only if that module is loaded
+	 *
+	 * @param array $testModules The array of registered test modules
+	 * @param ResourceLoader $resourceLoader The reference to the resource loader
+	 * @return true
+	 */
+	public static function onResourceLoaderTestModules(
+		array &$testModules,
+		ResourceLoader &$resourceLoader
+	) {
+		$resourceModules = $resourceLoader->getConfig()->get( 'ResourceModules' );
+
+		if (
+			isset( $resourceModules[ 'ext.cite.visualEditor' ] ) ||
+			$resourceLoader->isModuleRegistered( 'ext.cite.visualEditor' )
+		) {
+			$testModules['qunit']['ext.cite.visualEditor.test'] = array(
+				'scripts' => array(
+					'modules/ve-cite/tests/ve.dm.citeExample.js',
+					'modules/ve-cite/tests/ve.dm.Converter.test.js',
+					'modules/ve-cite/tests/ve.dm.InternalList.test.js',
+					'modules/ve-cite/tests/ve.dm.Transaction.test.js',
+				),
+				'dependencies' => array(
+					'ext.cite.visualEditor',
+					'ext.visualEditor.test'
+				),
+				'localBasePath' => __DIR__,
+				'remoteExtPath' => 'Cite',
+			);
+		}
+
+		return true;
+	}
 }

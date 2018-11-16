9c4620966f318f7b224be6e948ed444cc4d4fcb1
WMDE-Fisch <christoph.jauera@wikimedia.de>
2018-11-16 12:55:11 +0100

    Add browser test for highlighting of jump mark symbols
    Bug: T205270
    Change-Id: Ieccf2c507dafac70d287eb0fa0fff051442205da

--- tests/selenium/specs/cite.js
+++ tests/selenium/specs/cite.js
@@ -1,9 +1,38 @@
 var assert = require( 'assert' ),
+	Api = require( 'wdio-mediawiki/Api' ),
+	CitePage = require( '../pageobjects/cite.page' ),
+	Util = require( 'wdio-mediawiki/Util' ),
 	VersionPage = require( '../pageobjects/version.page' );
 
 describe( 'Cite', function () {
+	var title;
+
+	before( function () {
+		title = Util.getTestString( 'CiteTest-title-' );
+
+		browser.call( function () {
+			return Api.edit(
+				title,
+				'abc<ref name="a">reftext</ref>\n\ndef<ref name="a" />\n\n<references />'
+			);
+		} );
+	} );
+
 	it( 'is configured correctly', function () {
 		VersionPage.open();
 		assert( VersionPage.extension.isExisting() );
+	} );
+
+	it( 'highlights the jump mark symbol in the reference list', function () {
+		CitePage.openTitle( title );
+
+		browser.pause( 300 ); // make sure JS is loaded
+
+		CitePage.getCiteLink( 2 ).click();
+		assert(
+			CitePage.getCiteSubBacklink( 2 ).getAttribute( 'class' )
+				.indexOf( 'mw-cite-targeted-backlink' ) !== -1,
+			'the jump mark symbol of the backlink is highlighted'
+		);
 	} );
 } );

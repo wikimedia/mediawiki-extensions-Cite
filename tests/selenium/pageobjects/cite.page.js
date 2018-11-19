89deb7ca4cc41fedac634c592ec3f852272434f6
WMDE-Fisch <christoph.jauera@wikimedia.de>
2018-11-19 17:00:32 +0100

    Add browser tests for linking the main backlink
    This also includes putting the backling tests in their
    own file due to the common setup needed.
    
    Bug: T205271
    Change-Id: Ica98c31bf0fddde9eb80f9c53b3dc089cb60e30c

--- tests/selenium/pageobjects/cite.page.js
+++ tests/selenium/pageobjects/cite.page.js
@@ -1,8 +1,15 @@
 const Page = require( 'wdio-mediawiki/Page' );
 
 class CitePage extends Page {
-	getCiteLink( num ) { return browser.elements( '#mw-content-text .reference a' ).value[ num - 1 ]; }
+	getReference( num ) { return browser.elements( '#mw-content-text .reference' ).value[ num - 1 ]; }
+	getCiteBacklink( num ) { return browser.element( '.references li:nth-of-type(' + num + ') .mw-cite-up-arrow-backlink' ); }
 	getCiteSubBacklink( num ) { return browser.element( '.mw-cite-backlink sup:nth-of-type(' + num + ') a' ); }
+
+	getFragmentFromLink( linkElement ) {
+		// the href includes the full url so slice the fragment from it
+		let href = linkElement.getAttribute( 'href' );
+		return href.slice( href.indexOf( '#' ) + 1 );
+	}
 }
 
 module.exports = new CitePage();

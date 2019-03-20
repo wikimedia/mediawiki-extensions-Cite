3c855ea4cd0ac787dc9b1eabe8a63ee25c1ccd8a
WMDE-Fisch <christoph.jauera@wikimedia.de>
2019-03-20 16:31:00 +0100

    Add test for broken up-arrow back links
    Bug: T217767
    Change-Id: Ib8c8e7d6cc286aaabd73f3e2b6fb02cb5f62eff2

--- tests/selenium/pageobjects/cite.page.js
+++ tests/selenium/pageobjects/cite.page.js
@@ -2,7 +2,8 @@ const Page = require( 'wdio-mediawiki/Page' );
 
 class CitePage extends Page {
 	getReference( num ) { return browser.elements( '#mw-content-text .reference' ).value[ num - 1 ]; }
-	getCiteBacklink( num ) { return browser.element( '.references li:nth-of-type(' + num + ') .mw-cite-up-arrow-backlink' ); }
+	getCiteMultiBacklink( num ) { return browser.element( '.references li:nth-of-type(' + num + ') .mw-cite-up-arrow-backlink' ); }
+	getCiteSingleBacklink( num ) { return browser.element( '.references li:nth-of-type(' + num + ') .mw-cite-backlink a' ); }
 	getCiteSubBacklink( num ) { return browser.element( '.mw-cite-backlink sup:nth-of-type(' + num + ') a' ); }
 
 	getFragmentFromLink( linkElement ) {

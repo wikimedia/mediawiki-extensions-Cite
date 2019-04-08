b6075be8ec3790cccde75041927c893c07a1a605
WMDE-Fisch <christoph.jauera@wikimedia.de>
2019-04-08 14:58:27 +0200

    Removing Special:Version test
    This is not really testing anything from the Cite extension.
    
    Change-Id: Iea8244527bfdbd21053a4f67295edc924c9ae725

--- tests/selenium/specs/cite.js
+++
@@ -1,9 +0,0 @@
-var assert = require( 'assert' ),
-	VersionPage = require( '../pageobjects/version.page' );
-
-describe( 'Cite', function () {
-	it( 'is configured correctly', function () {
-		VersionPage.open();
-		assert( VersionPage.extension.isExisting() );
-	} );
-} );

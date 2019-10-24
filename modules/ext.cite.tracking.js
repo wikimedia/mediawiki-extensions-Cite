f6b5615e4b4adf54ec5e5e43653d48c68159d6ce
Adam Wight <adam.wight@wikimedia.de>
2019-10-24 16:56:18 +0200

    Stop sampling when Reference Previews is enabled
    There are so few users with this configuration that we need to stop
    sampling in order to get data.  Sampling is still in effect for
    "baseline" users with the feature disabled.
    
    We use separate schemas to simplify analytics on the two populations.
    
    Bug: T214493
    Change-Id: I16e4ed236e50e1e246ff28ff0dba3e52e4b56caf

--- modules/ext.cite.tracking.js
+++ modules/ext.cite.tracking.js
@@ -1,23 +1,29 @@
 /**
- * @file Temporary tracking to establish a baseline for ReferencePreviews metrics.
+ * @file Temporary tracking to evaluate the impact of Reference Previews on users' interaction with references.
+ *
+ * The baseline metrics are for a sample of users who don't have ReferencePreviews enabled.
+ *
+ * Users with the feature enabled are not sampled, and events are logged using the ReferencePreviewsCite schema.
+ *
+ * @see https://phabricator.wikimedia.org/T214493
  * @see https://phabricator.wikimedia.org/T231529
  * @see https://meta.wikimedia.org/wiki/Schema:ReferencePreviewsBaseline
+ * @see https://meta.wikimedia.org/wiki/Schema:ReferencePreviewsCite
  */
 ( function () {
 	'use strict';
 
-	if ( navigator.sendBeacon && mw.eventLog.eventInSample( 1000 ) ) {
-		$( function () {
-			var isReferencePreviewsEnabled = mw.config.get( 'wgPopupsReferencePreviews', false );
+	$( function () {
+		var isReferencePreviewsEnabled = mw.config.get( 'wgPopupsReferencePreviews', false ),
+			loggingTopic = isReferencePreviewsEnabled ?
+				'event.ReferencePreviewsCite' :
+				'event.ReferencePreviewsBaseline',
+			samplingRate = isReferencePreviewsEnabled ?
+				1 :
+				1000,
+			isSampling = navigator.sendBeacon && mw.eventLog.eventInSample( samplingRate );
 
-			/**
-			 * @param {Object} event
-			 */
-			function logEvent( event ) {
-				event.referencePreviewsEnabled = isReferencePreviewsEnabled;
-				mw.track( 'event.ReferencePreviewsBaseline', event );
-			}
-
+		if ( isSampling ) {
 			// eslint-disable-next-line no-jquery/no-global-selector
 			$( '#mw-content-text' ).on(
 				'click',
@@ -25,7 +31,7 @@
 				'.reference a[ href*="#" ], .mw-reference-text a, .reference-text a',
 				function () {
 					var isInReferenceBlock = $( this ).parents( '.references' ).length > 0;
-					logEvent( {
+					mw.track( loggingTopic, {
 						action: ( isInReferenceBlock ?
 							'clickedReferenceContentLink' :
 							'clickedFootnote' )
@@ -33,7 +39,7 @@
 				}
 			);
 
-			logEvent( { action: 'pageview' } );
-		} );
-	}
+			mw.track( loggingTopic, { action: 'pageview' } );
+		}
+	} );
 }() );

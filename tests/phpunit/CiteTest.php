7965659b82b56180e8f62c232df638b1b774f26e
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2019-11-07 13:14:05 +0100

    Add dedicated unit test for Cite::refArg()
    Again, this is intentionally testing a private method.
    
    Change-Id: I559b88e38f7a7a4128ba0b16ff3de42f2fab2055

--- tests/phpunit/CiteTest.php
+++ tests/phpunit/CiteTest.php
@@ -11,8 +11,47 @@ class CiteTest extends MediaWikiTestCase {
 		parent::setUp();
 
 		$this->setMwGlobals( [
+			'wgCiteBookReferencing' => true,
 			'wgFragmentMode' => [ 'html5' ],
 		] );
+	}
+
+	/**
+	 * @covers ::refArg
+	 * @dataProvider provideRefAttributes
+	 */
+	public function testRefArg( array $attributes, array $expected ) {
+		/** @var Cite $cite */
+		$cite = TestingAccessWrapper::newFromObject( new Cite() );
+		$this->assertSame( $expected, $cite->refArg( $attributes ) );
+	}
+
+	public function provideRefAttributes() {
+		return [
+			[ [], [ null, null, false, null ] ],
+
+			// One attribute only
+			[ [ 'dir' => 'invalid' ], [ null, null, false, '' ] ],
+			[ [ 'dir' => 'rtl' ], [ null, null, false, ' class="mw-cite-dir-rtl"' ] ],
+			[ [ 'follow' => 'f' ], [ null, null, 'f', null ] ],
+			[ [ 'group' => 'g' ], [ null, 'g', null, null ] ],
+			[ [ 'invalid' => 'i' ], [ false, false, false, false ] ],
+			[ [ 'name' => 'n' ], [ 'n', null, null, null ] ],
+			[ [ 'name' => null ], [ false, false, false, false ] ],
+			[ [ 'refines' => 'r' ], [ null, null, null, null ] ],
+
+			// Pairs
+			[ [ 'follow' => 'f', 'name' => 'n' ], [ false, false, false, false ] ],
+			[ [ 'follow' => null, 'name' => null ], [ false, false, false, false ] ],
+			[ [ 'follow' => 'f', 'refines' => 'r' ], [ null, null, 'f', null ] ],
+			[ [ 'group' => 'r', 'name' => 'n' ], [ 'n', 'r', null, null ] ],
+
+			// Combinations of 3 or more attributes
+			[
+				[ 'group' => 'r', 'name' => 'n', 'refines' => 'f', 'dir' => 'rtl' ],
+				[ 'n', 'r', null, ' class="mw-cite-dir-rtl"' ]
+			],
+		];
 	}
 
 	/**

cad4d18458e53ae77429cf2f8760fef29f64bb41
Adam Wight <adam.wight@wikimedia.de>
2019-12-10 16:28:02 +0100

    Integration test to hit cloned Cite bug
    Tickle very particular edge case in which a recursive parse corrupts
    the $parser->extCite object.
    
    Bug: T240248
    Change-Id: I70d100e88fa72825194ed9c477b030bbf0b6b486

---
+++ tests/phpunit/CiteDbTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Cite\Tests;
+
+use MediaWiki\MediaWikiServices;
+use ParserOptions;
+use Title;
+
+/**
+ * @group Database
+ *
+ * @license GPL-2.0-or-later
+ */
+class CiteDbTest extends \MediaWikiIntegrationTestCase {
+
+	/**
+	 * Edge case where a parser call within `<ref>` parse clears the original parser state.
+	 * @covers \Cite\ReferenceStack::pushRef
+	 */
+	public function testReferenceStackError() {
+		$this->insertPage( 'Cite-tracking-category-cite-error', '{{PAGENAME}}', NS_MEDIAWIKI );
+
+		$services = MediaWikiServices::getInstance();
+		// Reset the MessageCache in order to force it to clone a new parser.
+		$services->resetServiceForTesting( 'MessageCache' );
+		$services->getMessageCache()->enable();
+
+		$parserOutput = $services->getParser()->parse(
+			'
+				<ref name="a">text #1</ref>
+				<ref name="a">text #2</ref>
+				<ref>text #3</ref>
+			',
+			Title::makeTitle( NS_MAIN, mt_rand() ),
+			ParserOptions::newFromAnon()
+		);
+
+		$this->assertContains(
+			'cite_ref-2',
+			$parserOutput->getText(),
+			'Internal counter should not reset to 1 for text #3'
+		);
+	}
+
+}

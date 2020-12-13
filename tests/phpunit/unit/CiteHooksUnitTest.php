35b704dc2f1c6bf7b607311eee83b8e00389a6a1
Ed Sanders <esanders@wikimedia.org>
2020-12-13 17:56:00 +0000

    Move ext.cite.ux-enhancements to extension.json
    Change-Id: Ia6a0c34800b018e76b9e246898ddfb991f238d55

--- tests/phpunit/unit/CiteHooksUnitTest.php
+++ tests/phpunit/unit/CiteHooksUnitTest.php
@@ -3,19 +3,15 @@
 namespace Cite\Tests\Unit;
 
 use Cite\Hooks\CiteHooks;
-use ResourceLoader;
 use Title;
 
 /**
- * @coversDefaultClass \Cite\Hooks\CiteHooks
+ * @covers \Cite\Hooks\CiteHooks
  *
  * @license GPL-2.0-or-later
  */
 class CiteHooksUnitTest extends \MediaWikiUnitTestCase {
 
-	/**
-	 * @covers ::onContentHandlerDefaultModelFor
-	 */
 	public function testOnContentHandlerDefaultModelFor() {
 		$title = $this->createMock( Title::class );
 		$title->method( 'inNamespace' )
@@ -26,17 +22,6 @@ class CiteHooksUnitTest extends \MediaWikiUnitTestCase {
 		CiteHooks::onContentHandlerDefaultModelFor( $title, $model );
 
 		$this->assertSame( CONTENT_MODEL_JSON, $model );
-	}
-
-	/**
-	 * @covers ::onResourceLoaderRegisterModules
-	 */
-	public function testOnResourceLoaderRegisterModules() {
-		$resourceLoader = $this->createMock( ResourceLoader::class );
-		$resourceLoader->expects( $this->atLeastOnce() )
-			->method( 'register' );
-
-		CiteHooks::onResourceLoaderRegisterModules( $resourceLoader );
 	}
 
 }

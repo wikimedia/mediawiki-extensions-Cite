00147fb1fe84b1bcb5b1af1076f1606cffb31ea9
WMDE-Fisch <christoph.jauera@wikimedia.de>
2024-03-15 19:15:57 +0100

    [Cypress] Split before steps to fix retries
    When the tests run into an issue and are re-run only beforeEach
    will be executed. Also cookies are not kept for the re-run.
    
    This patch makes sure that steps needed to have a clean re-run are
    split to the beforeEach
    
    Change-Id: I47cba7f6ec240fefa09a4484bca0a621b99f2bcb

--- cypress/e2e/tests/ve-cite/veCiteIntegration.cy.js
+++ cypress/e2e/tests/ve-cite/veCiteIntegration.cy.js
@@ -1,29 +1,34 @@
 import * as helpers from './../../utils/functions.helper.js';
-
 const title = helpers.getTestString( 'CiteTest-title' );
+
+const refText1 = 'This is citation #1 for reference #1 and #2';
+const wikiText = `This is reference #1: <ref name="a">${ refText1 }</ref><br> ` +
+	'This is reference #2 <ref name="a" /><br>' +
+	'<references />';
 
 describe( 'Visual Editor Cite Integration', () => {
 	before( () => {
 		cy.visit( '/index.php' );
-
-		const wikiText = 'This is reference #1: <ref name="a">This is citation #1 for reference #1 and #2</ref><br> ' +
-			'This is reference #2: <ref name="a" /><br>' +
-			'<references />';
-
 		helpers.editPage( title, wikiText );
+	} );
+
+	beforeEach( () => {
+		helpers.visitTitle( title );
+
 		cy.window().then( async ( win ) => {
 			win.localStorage.setItem( 've-beta-welcome-dialog', 1 );
 		} );
+
+		helpers.visitTitle( title, { veaction: 'edit' } );
 	} );
 
 	it( 'should edit and verify reference content in Visual Editor', () => {
-		helpers.visitTitle( title, { veaction: 'edit' } );
-
 		helpers.getVEFootnoteMarker( 'a', 1, 1 ).click();
+
 		// Popup appears containing ref content
 		helpers.getVEReferencePopup()
 			.should( 'be.visible' )
-			.should( 'have.text', 'This is citation #1 for reference #1 and #2' );
+			.should( 'contain.text', refText1 );
 
 		// Open edit popup
 		cy.contains( '.oo-ui-buttonElement-button', 'Edit' ).click();
@@ -31,7 +36,7 @@ describe( 'Visual Editor Cite Integration', () => {
 		// Dialog appears with ref content
 		helpers.getVEDialog()
 			.should( 'be.visible' )
-			.should( 'have.text', 'This is citation #1 for reference #1 and #2' );
+			.should( 'contain.text', refText1 );
 	} );
 
 } );

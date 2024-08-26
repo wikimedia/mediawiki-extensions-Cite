8ac56d3eb43fbed49d10b9c3474acc8dc5ff8d57
thiemowmde <thiemo.kreuz@wikimedia.de>
2024-08-26 11:22:36 +0200

    Rename "BookReferencing" flag exposed in JS
    We can do this independent from the flag in PHP. See T373307 for
    details.
    
    Bug: T373307
    Bug: T385581
    Change-Id: I0b47562f34300b9b054c1f51c3c469a853ff92bb
    (cherry picked from commit 1e907cc89afa84088356869f1080ca67088ac617)

--- tests/phpunit/CiteHooksTest.php
+++ tests/phpunit/CiteHooksTest.php
@@ -39,7 +39,7 @@ class CiteHooksTest extends \MediaWikiIntegrationTestCase {
 		$this->assertSame( [
 			'wgCiteVisualEditorOtherGroup' => $enabled,
 			'wgCiteResponsiveReferences' => $enabled,
-			'wgCiteBookReferencing' => $enabled,
+			'wgCiteSubReferencing' => $enabled,
 		], $vars );
 	}
 

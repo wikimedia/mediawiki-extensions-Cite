5600b1fc4749736ee0b6d7c3d09d8c3c7e239f33
Kosta Harlan <kharlan@wikimedia.org>
2024-10-30 20:05:39 +0100

    wt2017Integration.cy.js: Disable flaky test
    Bug: T377666
    Change-Id: I34de82574e172359a334e8e8c4ef7382a92e5509

--- tests/cypress/e2e/tests/ve-cite/wt2017Integration.cy.js
+++ tests/cypress/e2e/tests/ve-cite/wt2017Integration.cy.js
@@ -69,7 +69,8 @@ describe( 'Visual Editor Wt 2017 Cite Integration', () => {
 
 	} );
 
-	it( 'should be able to create a VE-Cite tool template', () => {
+	// Disable flaky test T377666
+	it.skip( 'should be able to create a VE-Cite tool template', () => {
 		// FIXME: Replace this wait with a trigger when VE is fully initialized.
 		cy.wait( 1000 );
 		if ( usesCitoid ) {

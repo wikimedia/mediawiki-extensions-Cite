e1190582f553a96a2554c43a5b481ff2da6e5cc6
WMDE-Fisch <christoph.jauera@wikimedia.de>
2025-01-30 14:58:36 +0100

    CommunityConfiguration: Add feature flag to hide Cite provider
    I'm using the CommunityConfiguration hook here to disable the
    provider.  Inspired by what was done in the Babel extension to do
    the same.
    
    The utility method was introduced for convinience because we also
    can use it to manage the fallback handling.
    
    Bug: T385158
    Change-Id: I7e6067204c3850bf90757c6702309e503bdb09f4

--- .phan/config.php
+++ .phan/config.php
@@ -6,6 +6,7 @@ $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'],
 	[
 		'../../extensions/cldr',
+		'../../extensions/CommunityConfiguration',
 		'../../extensions/Gadgets',
 	]
 );
@@ -14,6 +15,7 @@ $cfg['exclude_analysis_directory_list'] = array_merge(
 	$cfg['exclude_analysis_directory_list'],
 	[
 		'../../extensions/cldr',
+		'../../extensions/CommunityConfiguration',
 		'../../extensions/Gadgets',
 	]
 );

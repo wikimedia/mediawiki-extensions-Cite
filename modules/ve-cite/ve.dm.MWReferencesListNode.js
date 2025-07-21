133da20f8fe57d0e3a632590457d43e575a4befe
WMDE-Fisch <christoph.jauera@wikimedia.de>
2025-07-21 06:38:47 +0000

    Revert "VE: Enforce referenceslist reserialization when MW changed"
    This reverts commit 9f15f417da7a5e1cb4bc098e9fc6895cd610405a.
    
    Seems to have caused https://phabricator.wikimedia.org/T400013
    
    Bug: T400013
    Bug: T396017
    Change-Id: I3a37e31f17d803cf08e4091e37478de726e1e2ef

--- modules/ve-cite/ve.dm.MWReferencesListNode.js
+++ modules/ve-cite/ve.dm.MWReferencesListNode.js
@@ -162,8 +162,6 @@ ve.dm.MWReferencesListNode.static.toDomElements = function ( data, doc, converte
 			.toDomElements.call( this, dataElement, doc, converter );
 	}
 
-	const updatedMw = ve.dm.MWReferencesListNode.static.updatedMwForDom( data, doc, converter );
-
 	let domElements = [ doc.createElement( 'div' ) ];
 	if ( !converter.isForParser() ) {
 		// Output needs to be read so re-render
@@ -177,7 +175,7 @@ ve.dm.MWReferencesListNode.static.toDomElements = function ( data, doc, converte
 		// Destroy the view node so it doesn't try to update the DOM node later
 		// (e.g. updateDebounced)
 		viewNode.destroy();
-	} else if ( dataElement.originalDomElementsHash !== undefined && !updatedMw ) {
+	} else if ( dataElement.originalDomElementsHash !== undefined ) {
 		// If there's more than 1 element, preserve entire array, not just first element
 		domElements = ve.copyDomElements(
 			converter.getStore().value( dataElement.originalDomElementsHash ), doc
@@ -202,7 +200,7 @@ ve.dm.MWReferencesListNode.static.toDomElements = function ( data, doc, converte
 	domElements[ 0 ].setAttribute( 'typeof', 'mw:Extension/references' );
 	domElements[ 0 ].setAttribute(
 		'data-mw',
-		!updatedMw ? dataElement.attributes.originalMw : updatedMw
+		ve.dm.MWReferencesListNode.static.buildMwForDom( data, doc, converter )
 	);
 
 	return domElements;
@@ -215,9 +213,9 @@ ve.dm.MWReferencesListNode.static.toDomElements = function ( data, doc, converte
  * @param {Object} data
  * @param {Document} doc
  * @param {ve.dm.Converter} converter
- * @return {string|false} updated mwData as string or false if nothing changed
+ * @return {string} updated mwData as string or originalMw if nothing changed
  * */
-ve.dm.MWReferencesListNode.static.updatedMwForDom = function ( data, doc, converter ) {
+ve.dm.MWReferencesListNode.static.buildMwForDom = function ( data, doc, converter ) {
 	const attributes = data[ 0 ].attributes;
 	const originalMw = attributes.originalMw;
 	const originalMwData = originalMw && JSON.parse( originalMw );
@@ -275,7 +273,7 @@ ve.dm.MWReferencesListNode.static.updatedMwForDom = function ( data, doc, conver
 
 	// If mwData and originalMw are the same, use originalMw to prevent reserialization.
 	// Reserialization has the potential to reorder keys and so change the DOM unnecessarily
-	return originalMw && ve.compare( mwData, originalMwData ) ? false : JSON.stringify( mwData );
+	return originalMw && ve.compare( mwData, originalMwData ) ? originalMw : JSON.stringify( mwData );
 };
 
 /***

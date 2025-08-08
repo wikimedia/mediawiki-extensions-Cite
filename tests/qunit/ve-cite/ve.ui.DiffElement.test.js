86c30e63fb6eed04deec88145283c20383c9d666
Adam Wight <adam.wight@wikimedia.de>
2025-08-08 07:16:53 +0200

    Add test that demonstrates a renumbered ref
    The output looks good, and it makes sense that renumbering isn't
    shown in the diff.  Instead, the old numbers are shown for removed
    text and the new numbers are shown on unchanged text.
    
    This test case exercises the `overrideIndex` code paths.
    
    Change-Id: I547fdaed89008c9f24d8fd75bcf136c397407017

--- tests/qunit/ve-cite/ve.ui.DiffElement.test.js
+++ tests/qunit/ve-cite/ve.ui.DiffElement.test.js
@@ -43,6 +43,28 @@ QUnit.test( 'Diffing', ( assert ) => {
 						<li value="3"><p data-diff-action="none">Baz</p></li>
 					</ol>
 				`
+			},
+			{
+				msg: 'Renumber ref',
+				oldDoc: ve.dm.example.singleLine`
+					<p>A ${ ref( 'Foo' ) }</p>
+					<p>B ${ ref( 'Bar' ) }</p>
+					<p>C ${ ref( 'Baz' ) }</p>
+				`,
+				newDoc: ve.dm.example.singleLine`
+					<p>A ${ ref( 'Foo' ) }</p>
+					<p>C ${ ref( 'Baz' ) }</p>
+				`,
+				expected: ve.dm.example.singleLine`
+					<p data-diff-action=\"none\">A ${ ref( 'Foo', 1 ) }</p>
+					<p data-diff-action=\"remove\">B ${ ref( 'Bar', 2 ) }</p>
+					<p data-diff-action=\"none\">C ${ ref( 'Baz', 2 ) }</p>
+					<ol>
+						<li value=\"1\"><p data-diff-action=\"none\">Foo</p></li>
+						<li value=\"2\"><p data-diff-action=\"remove\">Bar</p></li>
+						<li value=\"2\"><p data-diff-action=\"none\">Baz</p></li>
+					</ol>
+				`
 			}
 		];
 

86916c7b13d17ad343b6a5134cc5f43ab94abc17
WMDE-Fisch <christoph.jauera@wikimedia.de>
2025-10-27 16:12:42 +0100

    VE: Simplify ve.dm.Transaction test runner
    I mainly did these things in this change to improve the readability
    of this QUnit test:
    
    A few annotations, better variable names, less complex indentation.
    
    Removal of dead code in the "test runner" part.  The only tests
    case in this file did neither test the removal of data nor the
    data in the hash store.  So both parts of the test runner can be
    removed.
    
    In another step I might reduce the complexity of the test case
    itself.  But I think, this is a good start already to better get
    what's going on.
    
    Motivated by the failing tests after merging
    I9e501ce1228edfefc01a1980b7670ad591f7ac69
    
    Bug: T408535
    Change-Id: Ie242b53268449c4b1203d25d2cb955cd5e963648

--- tests/qunit/ve-cite/ve.dm.Transaction.test.js
+++ tests/qunit/ve-cite/ve.dm.Transaction.test.js
@@ -11,95 +11,82 @@ QUnit.module( 've.dm.Transaction (Cite)', ve.test.utils.newMwEnvironment() );
 
 // FIXME: Duplicates test runner; should be using a data provider
 QUnit.test( 'newFromDocumentInsertion with references', ( assert ) => {
-	const complexDoc = ve.dm.citeExample.createExampleDocument( 'complexInternalData' ),
-		withReference = [
-			{ type: 'paragraph' },
-			'B', 'a', 'r',
-			{ type: 'mwReference', attributes: {
-				mw: {},
-				about: '#mwt4',
-				listIndex: 0,
-				listGroup: 'mwReference/',
-				listKey: 'auto/0',
-				refGroup: '',
-				contentsUsed: true
-			} },
-			{ type: '/mwReference' },
-			{ type: '/paragraph' },
-			{ type: 'internalList' },
-			{ type: 'internalItem' },
-			{ type: 'paragraph', internal: { generated: 'wrapper' } },
-			'B',
-			'a',
-			'z',
-			{ type: '/paragraph' },
-			{ type: '/internalItem' },
-			{ type: '/internalList' }
-		],
-		cases = [
-			{
-				msg: 'inserting a brand new document; internal lists are merged and items renumbered',
-				doc: 'complexInternalData',
-				offset: 7,
-				newDocData: withReference,
-				removalOps: [],
-				expectedOps: [
-					{ type: 'retain', length: 7 },
-					{
-						type: 'replace',
-						remove: [],
-						insert: withReference.slice( 0, 4 )
-							// Renumber listIndex from 0 to 2
-							// Renumber listKey from auto/0 to auto/1
-							.concat( [
-								ve.extendObject( true, {}, withReference[ 4 ],
-									{ attributes: { listIndex: 2, listKey: 'auto/1' } }
-								)
-							] )
-							.concat( withReference.slice( 5, 7 ) )
-					},
-					{ type: 'retain', length: 1 },
-					{
-						type: 'replace',
-						remove: complexDoc.getData( new ve.Range( 8, 32 ) ),
-						insert: complexDoc.getData( new ve.Range( 8, 32 ) )
-							.concat( withReference.slice( 8, 15 ) )
-					},
-					{ type: 'retain', length: 1 }
-				]
-			}
-		];
+	// Fixtures
+	const complexDoc = ve.dm.citeExample.createExampleDocument( 'complexInternalData' );
+	const simpleRefDocData = [
+		{ type: 'paragraph' },
+		'B', 'a', 'r',
+		{ type: 'mwReference', attributes: {
+			mw: {},
+			about: '#mwt4',
+			listIndex: 0,
+			listGroup: 'mwReference/',
+			listKey: 'auto/0',
+			refGroup: '',
+			contentsUsed: true
+		} },
+		{ type: '/mwReference' },
+		{ type: '/paragraph' },
+		{ type: 'internalList' },
+		{ type: 'internalItem' },
+		{ type: 'paragraph', internal: { generated: 'wrapper' } },
+		'B',
+		'a',
+		'z',
+		{ type: '/paragraph' },
+		{ type: '/internalItem' },
+		{ type: '/internalList' }
+	];
 
+	// Test cases
+	const cases = [
+		{
+			msg: 'inserting a brand new document; internal lists are merged and items renumbered',
+			doc: 'complexInternalData',
+			offset: 7,
+			newDocData: simpleRefDocData,
+			expectedOps: [
+				{ type: 'retain', length: 7 },
+				{
+					type: 'replace',
+					remove: [],
+					insert: simpleRefDocData.slice( 0, 4 )
+						// Renumber listIndex from 0 to 2
+						// Renumber listKey from auto/0 to auto/1
+						.concat( [
+							ve.extendObject( true, {}, simpleRefDocData[ 4 ],
+								{ attributes: { listIndex: 2, listKey: 'auto/1' } }
+							)
+						] )
+						.concat( simpleRefDocData.slice( 5, 7 ) )
+				},
+				{ type: 'retain', length: 1 },
+				{
+					type: 'replace',
+					remove: complexDoc.getData( new ve.Range( 8, 32 ) ),
+					insert: complexDoc.getData( new ve.Range( 8, 32 ) )
+						.concat( simpleRefDocData.slice( 8, 15 ) )
+				},
+				{ type: 'retain', length: 1 }
+			]
+		}
+	];
+
+	// Test runner
 	cases.forEach( ( caseItem ) => {
-		const doc = ve.dm.citeExample.createExampleDocument( caseItem.doc );
-		let doc2, removalOps;
+		const originalDoc = ve.dm.citeExample.createExampleDocument( caseItem.doc );
+		let otherDoc;
 		if ( caseItem.newDocData ) {
-			doc2 = new ve.dm.Document( caseItem.newDocData );
-			removalOps = [];
-		} else if ( caseItem.range ) {
-			doc2 = doc.cloneFromRange( caseItem.range );
-			caseItem.modify( doc2 );
-			const removalTx = ve.dm.TransactionBuilder.static
-				.newFromRemoval( doc, caseItem.range, true );
-			doc.commit( removalTx );
-			removalOps = removalTx.getOperations();
+			otherDoc = new ve.dm.Document( caseItem.newDocData );
 		}
 
-		assert.deepEqualWithDomElements(
-			removalOps, caseItem.removalOps, caseItem.msg + ': removal'
+		// Build transaction
+		const tx = ve.dm.TransactionBuilder.static.newFromDocumentInsertion(
+			originalDoc, caseItem.offset, otherDoc
 		);
 
-		const tx = ve.dm.TransactionBuilder.static.newFromDocumentInsertion(
-			doc, caseItem.offset, doc2
-		);
 		assert.deepEqualWithDomElements(
 			tx.getOperations(), caseItem.expectedOps, caseItem.msg + ': transaction'
 		);
-
-		const expectedStoreItems = caseItem.expectedStoreItems || [];
-		const actualStoreItems = expectedStoreItems.map(
-			( item ) => doc.store.value( OO.getHash( item ) )
-		);
-		assert.deepEqual( actualStoreItems, expectedStoreItems, caseItem.msg + ': store items' );
 	} );
 } );

48910230c6625a458dc8630447d286a4338addb3
Svantje Lilienthal <svantje.lilienthal@wikimedia.de>
2026-02-10 17:27:15 +0100

    Add instrument for clicks in TOC references link
    We want to gather some baseline data on how often users click the
    link in the table of contents to get to the references section.
    
    Here we're simply assuming that the references section at the very
    end is the most important one we're interested in.
    
    Bug: T415910
    Change-Id: I852e00ef9d678ab93ba1be50912512774d32ad63

---
+++ modules/ext.cite.track.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/**
+ * Adding code for analytic instruments
+ */
+( function () {
+	/**
+	 * @param {jQuery} $content
+	 * @param {MetricsPlatform.Instrument} instrument
+	 */
+	function addTocTracking( $content, instrument ) {
+		// Find the references list inside the page content
+		const $referencesList = $content.find( '.mw-heading2 ~ .mw-references-wrap, .mw-heading2 ~ ol.references' ).last();
+		const $headline = $referencesList.prevAll( '.mw-heading2' ).first().children( 'h2' );
+		const anchor = $headline.attr( 'id' );
+
+		if ( !anchor ) {
+			return;
+		}
+
+		// Find the TOC link that points to this anchor
+		const $tocLink = $( '#vector-toc a[href="#' + CSS.escape( anchor ) + '"]' );
+
+		// Ensure we only add the handler once
+		if ( $tocLink.data( 'toc-tracking-attached' ) ) {
+			return;
+		}
+		$tocLink.data( 'toc-tracking-attached', true );
+
+		// Add click handler
+		$tocLink.on( 'click', () => {
+			instrument.submitInteraction( 'click-toc-link' );
+		} );
+	}
+
+	/**
+	 * Adds temporary tracking for user interactions with footnote content T415904
+	 *
+	 * @param {jQuery} $content
+	 */
+	function addFootnoteContentInstrument( $content ) {
+		/** @type {MetricsPlatform.Instrument|undefined} */
+		const footNoteInteractionInstrument = mw.testKitchen &&
+		mw.testKitchen.getInstrument( 'cite-footnote-content-interaction' );
+
+		if ( footNoteInteractionInstrument && footNoteInteractionInstrument.isEnabled() ) {
+			addTocTracking( $content, footNoteInteractionInstrument );
+		}
+	}
+
+	// Adds tracking to content hook
+	mw.hook( 'wikipage.content' ).add( addFootnoteContentInstrument );
+}() );

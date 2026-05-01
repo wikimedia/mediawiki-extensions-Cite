4d8924ec0fdfa222bc25039f869ca6bc244788cd
WMDE-Fisch <christoph.jauera@wikimedia.de>
2026-05-01 09:56:29 +0200

    Cypress: Merge reuse tests
    It doesn't make sense to have more than one reuse test IMO.  So I
    merge the ones that are there into one.  The test touches all
    relevant parts to make sure the reuse widget is working correctly
    and that a reuse is added to the article as intended.
    
    Bug: T424334
    Change-Id: I56abf6a9819a7f6823ac2f61b04fa3b9f442b612

--- tests/cypress/e2e/tests/ve-cite/veCiteIntegration.cy.js
+++ tests/cypress/e2e/tests/ve-cite/veCiteIntegration.cy.js
@@ -29,7 +29,7 @@ describe( 'VisualEditor Cite', () => {
 		veHelper.openVEForEditingReferences( title, usesCitoid );
 	} );
 
-	it( 'should edit and verify reference content in Visual Editor', () => {
+	it( 'should be able to edit and verify reference content in Visual Editor', () => {
 		veHelper.getVEFootnoteMarker( 'a', 1, 1 ).click();
 
 		// Popup appears containing ref content
@@ -46,7 +46,13 @@ describe( 'VisualEditor Cite', () => {
 			.should( 'contain.text', refText1 );
 	} );
 
-	it( 'should display existing references in the Cite re-use dialog', () => {
+	it( 'should be able to reuse existing references via the reuse dialog', () => {
+		// Currently there are 3 refs in the article
+		helper.getRefsFromArticleSection().should( 'have.length', 3 );
+
+		// Place cursor next to ref #2 in order to add reuse next to it
+		cy.contains( '.ve-ui-surface .mw-reflink-text', '[2]' ).type( '{rightarrow}' );
+
 		if ( usesCitoid ) {
 			veHelper.openVECitoidReuseDialog();
 
@@ -54,16 +60,46 @@ describe( 'VisualEditor Cite', () => {
 			veHelper.openVECiteReuseDialog();
 		}
 
-		// Assert reference content for the first reference
+		// Assert reference content and name for the first reference
 		veHelper.getCiteReuseDialogRefResultName( 1 ).should( 'have.text', 'a' );
 		veHelper.getCiteReuseDialogRefResultCitation( 1 ).should( 'have.text', '[1]' );
 		veHelper.getCiteReuseDialogRefText( 1 ).should( 'have.text', refText1 );
+		veHelper.getCiteReuseDialogRefResultName( 1 ).should( 'have.text', 'a' );
 
 		// Assert reference content for the second reference
 		veHelper.getCiteReuseDialogRefResultName( 2 ).should( 'have.text', '' );
 		veHelper.getCiteReuseDialogRefResultCitation( 2 ).should( 'have.text', '[2]' );
 		veHelper.getCiteReuseDialogRefText( 2 ).should( 'have.text', refText2 );
 
-	} );
+		// Reuse the second reference
+		veHelper.getCiteReuseDialogRefResult( 2 ).click();
 
+		// The context dialog on the second reference shows it's being used twice
+		cy.contains( '.ve-ui-surface .mw-reflink-text', '[2]' ).click();
+		cy.get( '.oo-ui-popupWidget-popup .ve-ui-mwReferenceContextItem-reuse' )
+			.should( 'have.text', 'Used twice' );
+
+		veHelper.saveEdits();
+
+		// ARTICLE SECTION
+		// Ref has been added to article, there are now 4 refs in the article
+		helper.getRefsFromArticleSection().should( 'have.length', 4 );
+
+		// Ref #2 appears twice in the article, corresponding IDs match the backlinks in
+		// the references section
+		helper.backlinksIdShouldMatchFootnoteId( 2, 0, 2 );
+		helper.backlinksIdShouldMatchFootnoteId( 3, 1, 2 );
+
+		// Both references have the same footnote number
+		cy.get( '#mw-content-text p sup a' ).eq( 2 ).should( 'have.text', '[2]' );
+		cy.get( '#mw-content-text p sup a' ).eq( 3 ).should( 'have.text', '[2]' );
+
+		// REFERENCES SECTION
+		// References section contains a list item for each reference
+		helper.getRefsFromReferencesSection().should( 'have.length', 2 );
+
+		// Ref content should match
+		helper.getRefFromReferencesSection( 1 ).should( 'contain', refText1 );
+		helper.getRefFromReferencesSection( 2 ).find( '.reference-text' ).should( 'have.text', refText2 );
+	} );
 } );

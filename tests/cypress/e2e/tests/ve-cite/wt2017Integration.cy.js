e384630c7710a59b161eabda3f747c5819374998
Marielle Volz <marielle.volz@gmail.com>
2026-05-15 17:24:41 +0100

    Add cite-tool-definition.json back into tests
    Iac18e18d696 removed the last case of the creation of
    cite-tool-definition.json which caused the Cite integration
    tests run in conjunction with Citoid extension to fail.
    
    This adds back in the definition to the two files that have
    failing tests, to ensure this file exists. 
    
    Bug: T425847
    Change-Id: I766e46a1a855b2d6a38666022304b58bba44945c

--- tests/cypress/e2e/tests/ve-cite/wt2017Integration.cy.js
+++ tests/cypress/e2e/tests/ve-cite/wt2017Integration.cy.js
@@ -12,6 +12,20 @@ describe( 'VisualEditor Cite with WT2017 Editor', () => {
 			cy.skipOn( !deps.visualEditor );
 			usesCitoid = deps.citoid;
 		} );
+
+		helper.loginAsAdmin();
+		helper.editPage( 'MediaWiki:Cite-tool-definition.json', JSON.stringify( [
+			{
+				name: 'web',
+				title: 'Webseite',
+				template: 'Internetquelle'
+			},
+			{
+				name: 'book',
+				title: 'Literatur',
+				template: 'Literatur'
+			}
+		] ) );
 	} );
 
 	beforeEach( () => {

d5c966287270b40858ee1e85ee8ed4be3dbbd5b9
WMDE-Fisch <christoph.jauera@wikimedia.de>
2026-05-18 14:46:21 +0200

    VE QUnit: Streamline ContextItem test
    We can use the node from the example document and merge the tests
    on the getInternalItem method.
    
    Change-Id: Ie46ab55dbf60a9239e2d766aa5a3a0854a7c1b5f

--- tests/qunit/ve-cite/ve.ui.MWReferenceContextItem.test.js
+++ tests/qunit/ve-cite/ve.ui.MWReferenceContextItem.test.js
@@ -3,9 +3,7 @@
 QUnit.module( 've.ui.MWReferenceContextItem (Cite)', ve.test.utils.newMwEnvironment() );
 
 QUnit.test( 'getInternalItemNode', ( assert ) => {
-	const doc = ve.dm.citeExample.createExampleDocument( 'references' );
-	// XXX: This is a regression test with a fragile setup. Please feel free to delete this test
-	// when you feel like it doesn't make sense to update it.
+	const doc = ve.dm.citeExample.createExampleDocument( 'simpleRef' );
 	const context = {
 		isMobile: () => false,
 		getSurface: () => ( {
@@ -14,28 +12,13 @@ QUnit.test( 'getInternalItemNode', ( assert ) => {
 		} )
 	};
 
-	const node = new ve.dm.MWReferenceNode();
-	node.setDocument( doc );
-	const item = new ve.ui.MWReferenceContextItem( context, node );
+	const emptyRefNode = new ve.dm.MWReferenceNode();
+	emptyRefNode.setDocument( doc );
+	let item = new ve.ui.MWReferenceContextItem( context, emptyRefNode );
+	assert.false( !!item.getInternalItemNode() );
 
-	assert.strictEqual( item.getInternalItemNode(), null );
-} );
-
-QUnit.test( 'getInternalItemNode: make sure we get a node', ( assert ) => {
-	const doc = ve.dm.citeExample.createExampleDocument( 'references' );
-	const context = {
-		isMobile: () => false,
-		getSurface: () => ( {
-			getModel: () => new ve.dm.Surface( doc ),
-			isReadOnly: () => false
-		} )
-	};
-
-	const node = new ve.dm.MWReferenceNode( {
-		attributes: { listIndex: 0 }
-	} );
-	node.setDocument( doc );
-	const item = new ve.ui.MWReferenceContextItem( context, node );
-
+	// We know exactly where the ref node is, grab it from the document.
+	const refNode = doc.getDocumentNode().children[ 0 ].children[ 1 ];
+	item = new ve.ui.MWReferenceContextItem( context, refNode );
 	assert.strictEqual( item.getInternalItemNode().getType(), 'internalItem' );
 } );

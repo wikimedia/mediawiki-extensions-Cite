7c83dd49ee535e0ae72971f7c62c574c63efb987
WMDE-Fisch <christoph.jauera@wikimedia.de>
2026-06-29 16:14:35 +0200

    Fix async loading in footnote click interaction experiment
    The "compat" mode we used her is not compatible with the way how
    we track our users, so we needed to change the mechanisme to get
    the experiment object.  This included changing the affected parts
    to work with the async interface.
    
    In addition to that we make sure, that the testkitchen module is
    loaded and ready to avioid race conditions when the ressource
    loader loads the modules in random order.
    
    To keep it simple I did not optimize away the code duplicaiton.
    The code will hopefully be removed in the very near future.
    
    Bug: T415904
    Change-Id: Ifab22605ec0334b2fb232b8ee91dc3cd7ea1a478

--- modules/ext.cite.referencePreviews/createReferencePreview.js
+++ modules/ext.cite.referencePreviews/createReferencePreview.js
@@ -59,16 +59,6 @@ function renderReferencePreview(
 
 	const el = TEMPLATE.content.cloneNode( true ).children[ 0 ];
 
-	let testKitchenExperiment = null;
-
-	// Test Kitchen experiment: cite-footnote-content-interaction-experiment (T123456)
-	if ( mw.testKitchen && !mw.config.get( 'wgMFMode' ) ) {
-		testKitchenExperiment = mw.testKitchen.compat.getExperiment( 'cite-footnote-content-interaction-experiment' );
-	}
-
-	// Treatment Group
-	const isRefPreviewReflistLinkEnabled = testKitchenExperiment && testKitchenExperiment.isAssignedGroup( 'treatment' );
-
 	replaceWith(
 		el.querySelector( '.mwe-popups-title-placeholder' ),
 		mw.html.escape( titleMsg.text() )
@@ -132,27 +122,37 @@ function renderReferencePreview(
 	settingsButton.append( settingsButtonLabel );
 	el.querySelector( '.mwe-popups-settings' ).appendChild( settingsButton );
 
-	// Treatment group behavior
-	if ( isRefPreviewReflistLinkEnabled ) {
-		const reflistLink = el.querySelector( '.mwe-popups-reflist-link' );
-		reflistLink.textContent = mw.msg( 'cite-reference-previews-reflist-link' );
+	function getExperiment() {
+		return mw.loader.using( 'ext.testKitchen' ).then(
+			() => mw.testKitchen.getExperiment( 'cite-footnote-content-interaction-experiment' )
+		);
+	}
 
-		// Jump to reflist
-		reflistLink.addEventListener( 'click', () => {
-			testKitchenExperiment.send( 'click-goto-references' );
+	// Test Kitchen experiment: cite-footnote-content-interaction-experiment (T123456)
+	if ( !mw.config.get( 'wgMFMode' ) ) {
+		getExperiment().then( ( experiment ) => {
+			if ( experiment && experiment.getAssignedGroup( 'treatment' ) ) {
+				const reflistLink = el.querySelector( '.mwe-popups-reflist-link' );
+				reflistLink.textContent = mw.msg( 'cite-reference-previews-reflist-link' );
 
-			const targetId = model.url ? model.url.slice( 1 ) : null;
-			const targetElement = document.getElementById( targetId );
+				// Jump to reflist
+				reflistLink.addEventListener( 'click', () => {
+					experiment.send( 'click-goto-references' );
 
-			// Remove highlight from previously highlighted reference when selecting a new footnote
-			const previousHighlight = document.querySelector( '.mwe-popups-ref-highlight' );
-			if ( previousHighlight ) {
-				previousHighlight.classList.remove( 'mwe-popups-ref-highlight' );
-			}
+					const targetId = model.url ? model.url.slice( 1 ) : null;
+					const targetElement = document.getElementById( targetId );
 
-			if ( targetElement ) {
-				targetElement.scrollIntoView();
-				targetElement.classList.add( 'mwe-popups-ref-highlight' );
+					// Remove highlight from previously highlighted reference when selecting a new footnote
+					const previousHighlight = document.querySelector( '.mwe-popups-ref-highlight' );
+					if ( previousHighlight ) {
+						previousHighlight.classList.remove( 'mwe-popups-ref-highlight' );
+					}
+
+					if ( targetElement ) {
+						targetElement.scrollIntoView();
+						targetElement.classList.add( 'mwe-popups-ref-highlight' );
+					}
+				} );
 			}
 		} );
 	}

cd4ecdccdc49682da91ca90bf7a3e07e9de8705a
WMDE-Fisch <christoph.jauera@wikimedia.de>
2026-09-04 11:47:34 +0200

    QUnit: Adding sub-ref case to the ClipboardHandler tests
    I'm adding basic sub-ref copy and paste cases the the ClipboardHandler
    test to show what's currently working and what's not working.
    
    Internal copy and paste produces correct results.  External copy
    and paste show issues with conflicting names and mixed up content.
    
    Also some reordering of the test structures in the other cases.
    
    Bug: T436349
    Change-Id: Ice9f22e34f8458447ad435c0e83dfb21d373675a

--- tests/qunit/ve-cite/ve.ce.ClipboardHandler.test.js
+++ tests/qunit/ve-cite/ve.ce.ClipboardHandler.test.js
@@ -12,14 +12,21 @@
 	QUnit.test( 'beforePaste/afterPaste', ( assert ) => {
 		const cases = [
 			{
+				msg: 'Legacy parser read mode references stripped',
 				documentHtml: '<p></p>',
 				rangeOrSelection: new ve.Range( 1 ),
-				pasteHtml: 'a<sup id="cite_ref-1" class="reference"><a href="./Article#cite_note-1">[1]</a></sup>b',
+				pasteHtml: ve.dm.example.singleLine`
+				a
+					<sup id="cite_ref-1" class="reference">
+						<a href="./Article#cite_note-1">[1]</a>
+					</sup>
+				b
+			`,
 				expectedRangeOrSelection: new ve.Range( 3 ),
-				expectedHtml: '<p>ab</p>',
-				msg: 'Leagcy parser read mode references stripped'
+				expectedHtml: '<p>ab</p>'
 			},
 			{
+				msg: 'Parsoid read mode references stripped',
 				documentHtml: '<p></p>',
 				rangeOrSelection: new ve.Range( 1 ),
 				pasteHtml: ve.dm.example.singleLine`
@@ -30,10 +37,10 @@
 				b
 			`,
 				expectedRangeOrSelection: new ve.Range( 3 ),
-				expectedHtml: '<p>ab</p>',
-				msg: 'Parsoid read mode references stripped'
+				expectedHtml: '<p>ab</p>'
 			},
 			{
+				msg: 'VE references not stripped',
 				documentHtml: '<p></p>',
 				rangeOrSelection: new ve.Range( 1 ),
 				pasteHtml: ve.dm.example.singleLine`
@@ -52,10 +59,11 @@
 					</sup>
 					b
 				</p>
-			`,
-				msg: 'VE references not stripped'
+			`
 			},
 			{
+				msg: 'VE external reference with conflicting name disambiguated',
+				// ref with name foo
 				documentHtml: ve.dm.example.singleLine`
 				<p>
 					a
@@ -65,6 +73,7 @@
 				</p>
 			`,
 				rangeOrSelection: new ve.Range( 4 ),
+				// ref with name foo
 				pasteHtml: ve.dm.example.singleLine`
 				b
 					<sup typeof="mw:Extension/ref" data-mw='{"name":"ref","body":{"html":"...another reference HTML..."},"attrs":{"name":"foo"}}' class="mw-ref reference" about="#mwt1" id="cite_ref-foo-0" rel="dc:references">
@@ -72,6 +81,7 @@
 					</sup>
 			`,
 				expectedRangeOrSelection: new ve.Range( 7 ),
+				// 2nd ref with name foo2
 				expectedHtml: ve.dm.example.singleLine`
 				<p>
 					a
@@ -83,10 +93,11 @@
 						<a><span class="mw-reflink-text"><span class="cite-bracket">[</span>2<span class="cite-bracket">]</span></span></a>
 					</sup>
 				</p>
-			`,
-				msg: 'VE external reference with conflicting name disambiguated'
+			`
 			},
 			{
+				msg: 'VE internal reference with same name deduplicated',
+				// ref with name foo
 				documentHtml: ve.dm.example.singleLine`
 				<p>
 					a
@@ -98,6 +109,7 @@
 				internalSourceRangeOrSelection: new ve.Range( 0, 6 ),
 				rangeOrSelection: new ve.Range( 6 ),
 				expectedRangeOrSelection: new ve.Range( 10 ),
+				// 2nd ref with same name
 				expectedHtml: ve.dm.example.singleLine`
 				<p>
 					a
@@ -111,8 +123,105 @@
 						<a><span class="mw-reflink-text"><span class="cite-bracket">[</span>1<span class="cite-bracket">]</span></span></a>
 					</sup>
 				</p>
+			`
+			},
+			{
+				msg: 'VE internal sub-reference with same name deduplicated',
+				// sub-ref with name book
+				documentHtml: ve.dm.example.singleLine`
+				<p>
+					a
+						<sup typeof="mw:Extension/ref" data-mw='{"name":"ref","attrs":{"details":"page 1","name":"book"},"body":{"id":"mw-reference-text-cite_note-2","html":"page 1"},"mainRef":"book","mainBody":"mw-reference-text-cite_note-book-1","mainBodyHtml":"...2nd reference HTML..."}' class="mw-ref reference">
+							<a><span class="mw-reflink-text"><span class="cite-bracket">[</span>1.1<span class="cite-bracket">]</span></span></a>
+						</sup>
+				</p>
 			`,
-				msg: 'VE internal reference with same name deduplicated'
+				internalSourceRangeOrSelection: new ve.Range( 0, 6 ),
+				rangeOrSelection: new ve.Range( 6 ),
+				expectedRangeOrSelection: new ve.Range( 10 ),
+				// 2nd ref with same name
+				expectedHtml: ve.dm.example.singleLine`
+				<p>
+					a
+						<sup typeof="mw:Extension/ref" data-mw='{"name":"ref","attrs":{"details":"page 1","name":"book"},"body":{"id":"mw-reference-text-cite_note-2","html":"page 1"},"mainRef":"book","mainBody":"mw-reference-text-cite_note-book-1","mainBodyHtml":"...2nd reference HTML..."}' class="mw-ref reference">
+							<a><span class="mw-reflink-text"><span class="cite-bracket">[</span>1.1<span class="cite-bracket">]</span></span></a>
+						</sup>
+				</p>
+				<p>
+					a
+					<sup typeof="mw:Extension/ref" data-mw='{"name":"ref","attrs":{"name":"book"},"body":{"html":"page 1"},"mainRef":"book"}' class="mw-ref reference">
+						<a><span class="mw-reflink-text"><span class="cite-bracket">[</span>1.1<span class="cite-bracket">]</span></span></a>
+					</sup>
+				</p>
+			`
+			},
+			{
+				msg: 'VE external sub-reference with conflicting name disambiguated',
+				// ref with name book
+				documentHtml: ve.dm.example.singleLine`
+				<p>
+					a
+						<sup typeof="mw:Extension/ref" data-mw='{"name":"ref","body":{"html":"...original reference HTML..."},"attrs":{"name":"book"}}' class="mw-ref reference">
+							<a><span class="mw-reflink-text"><span class="cite-bracket">[</span>1<span class="cite-bracket">]</span></span></a>
+						</sup>
+				</p>
+			`,
+				rangeOrSelection: new ve.Range( 4 ),
+				// sub-ref with name book
+				pasteHtml: ve.dm.example.singleLine`
+				b
+					<sup typeof="mw:Extension/ref" data-mw='{"name":"ref","attrs":{"details":"page 1","name":"book"},"body":{"id":"mw-reference-text-cite_note-2","html":"page 1"},"mainRef":"book","mainBody":"mw-reference-text-cite_note-book-1","mainBodyHtml":"...2nd reference HTML..."}' class="mw-ref reference">
+						<a><span class="mw-reflink-text"><span class="cite-bracket">[</span>1.1<span class="cite-bracket">]</span></span></a>
+					</sup>
+			`,
+				expectedRangeOrSelection: new ve.Range( 7 ),
+				// FIXME 2nd ref should be `book2` `...2nd reference HTML...` see #T418324
+				expectedHtml: ve.dm.example.singleLine`
+				<p>
+					a
+					<sup typeof="mw:Extension/ref" data-mw='{"name":"ref","body":{"html":"...original reference HTML..."},"attrs":{"name":"book"}}' class="mw-ref reference">
+						<a><span class="mw-reflink-text"><span class="cite-bracket">[</span>1<span class="cite-bracket">]</span></span></a>
+					</sup>
+					b
+					<sup typeof="mw:Extension/ref" data-mw='{"name":"ref","attrs":{"name":"book"},"body":{"html":"page 1"},"mainRef":"book","mainBodyHtml":"page 1"}' class="mw-ref reference">
+						<a><span class="mw-reflink-text"><span class="cite-bracket">[</span>2.1<span class="cite-bracket">]</span></span></a>
+					</sup>
+				</p>
+			`
+			},
+			{
+				msg: 'VE external reference with conflicting sub-reference name disambiguated',
+				// sub-ref with name book
+				documentHtml: ve.dm.example.singleLine`
+				<p>
+					a
+						<sup typeof="mw:Extension/ref" data-mw='{"name":"ref","attrs":{"details":"page 1","name":"book"},"body":{"id":"mw-reference-text-cite_note-2","html":"page 1"},"mainRef":"book","mainBody":"mw-reference-text-cite_note-book-1","mainBodyHtml":"...2nd reference HTML..."}' class="mw-ref reference">
+							<a><span class="mw-reflink-text"><span class="cite-bracket">[</span>1.1<span class="cite-bracket">]</span></span></a>
+						</sup>
+				</p>
+			`,
+				rangeOrSelection: new ve.Range( 4 ),
+				// ref with name book
+				pasteHtml: ve.dm.example.singleLine`
+				b
+					<sup typeof="mw:Extension/ref" data-mw='{"name":"ref","body":{"html":"...original reference HTML..."},"attrs":{"name":"book"}}' class="mw-ref reference">
+						<a><span class="mw-reflink-text"><span class="cite-bracket">[</span>1<span class="cite-bracket">]</span></span></a>
+					</sup>
+			`,
+				expectedRangeOrSelection: new ve.Range( 7 ),
+				// FIXME 2nd ref should be `book2` see #T418324
+				expectedHtml: ve.dm.example.singleLine`
+				<p>
+					a
+					<sup typeof="mw:Extension/ref" data-mw='{"name":"ref","attrs":{"details":"page 1","name":"book"},"body":{"id":"mw-reference-text-cite_note-2","html":"page 1"},"mainRef":"book","mainBody":"mw-reference-text-cite_note-book-1","mainBodyHtml":"...2nd reference HTML..."}' class="mw-ref reference">
+						<a><span class="mw-reflink-text"><span class="cite-bracket">[</span>1.1<span class="cite-bracket">]</span></span></a>
+					</sup>
+					b
+					<sup typeof="mw:Extension/ref" data-mw='{"name":"ref","body":{"html":"...original reference HTML..."},"attrs":{"name":"book"}}' class="mw-ref reference">
+						<a><span class="mw-reflink-text"><span class="cite-bracket">[</span>2<span class="cite-bracket">]</span></span></a>
+					</sup>
+				</p>
+			`
 			}
 		];
 
